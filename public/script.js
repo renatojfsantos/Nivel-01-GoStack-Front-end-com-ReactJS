b0a2c4dafa91a6d608a10c0f2232a6891b1bbb39
Renato Santos <renato_jfs@hotmail.com>
2020-11-09 00:01:13 -0300

    paste public

---
+++ public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (transpilar) codigo do React para um codigo que o browser entenda
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) eu vou converter o codigo de uma maneira diferente
+ * 
+ * Loaders: babel-loader, css-loader, image-loader
+ */
