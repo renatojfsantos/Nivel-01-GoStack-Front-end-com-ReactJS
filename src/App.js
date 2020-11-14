6e1dbe39463cfa0c3d62f01c1ed4ade9224650c0
Renato Santos <renato_jfs@hotmail.com>
2020-11-14 11:09:29 -0300

    New App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
+
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/backgroundImage.jpeg';
 
 import Header from './components/Header';
 
 /**
  * Componente
  * Propriedade
- * Estado e Imutabilidade
+ * Estado & Imutabilidade
  */
 
-  //useState retorna um arra com 2 posiçoes
-  //
-  //1. Variável com o seu valor inicial
-  //2. Função para atualizarmos esse valor
-
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  // useState retorna um array com 2 posições
+  //
+  // 1. Variável com seu valor inicial
+  // 2. Função para atualizarmos esse valor
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+  
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Danilo Vieira'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
     <>
       <Header title="Projects" />
 
-      <img width={300} src={backgroundImage} />
-
       <ul>
-        {projects.map(projects => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
-      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+      <button type="button" onClick={handleAddProject}></button>
     </>
   );
 }
